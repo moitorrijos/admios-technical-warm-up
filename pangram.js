827aef36032c684e03d4e079b7ffcf5822b42fcc
Juan Moises Torrijos <juanmtorrijos@Juans-MacBook-Air.local>
2021-08-25 10:20:55 -0500

    Fix regex to pass tests.

--- pangram.js
+++ pangram.js
@@ -1,6 +1,6 @@
 
 export const isPangram = (string) => {
-  const regex = new RegExp(/[^a-z ]+\D/g, '');
+  const regex = new RegExp(/[a-z ]+\D/gi, '');
   // return regex.test(string)
   const array = Array.from(new Set(string.replace(/[^a-z]/g, '')));
   if (array.length !== 26) return false
