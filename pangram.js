28bba040fbbf4b77a4e420c97ed31113ab1f611b
Juan Moises Torrijos <juanmtorrijos@Juans-MacBook-Air.local>
2021-08-25 11:05:49 -0500

    Final answer.

--- pangram.js
+++ pangram.js
@@ -1,8 +1,16 @@
 
 export const isPangram = (string) => {
-  const regex = new RegExp(/[a-z ]+\D/gi, '');
-  // return regex.test(string)
+  /* 
+   Create a new set from the string,
+   transform the set to an array 
+   and verify if the length of the array 
+   is as long as the letters from the alphabet.
+  */
   const array = Array.from(new Set(string.replace(/[^a-z]/g, '')));
   if (array.length !== 26) return false
+  /* 
+    Create a regular expression that matches the following
+  */
+  const regex = new RegExp(/.[a-z ]\w../, 'gi');
   return regex.test(string)
 };
