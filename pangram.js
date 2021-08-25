152b70904e03764f7b9d42b28bf1d5e7d6db24b0
Juan Moises Torrijos <juanmtorrijos@Juans-MacBook-Air.local>
2021-08-25 09:20:54 -0500

    Adds gitignore file. Adds a parameter to the isPangram parameter and checks if its a string.

--- pangram.js
+++ pangram.js
@@ -1,4 +1,8 @@
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = (string) => {
+  if (typeof string === 'string') {
+    console.log('string is a string');
+  } else {
+    console.log('string is not a string');
+   }
 };
