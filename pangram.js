acae3ee6a23659509ea8e565a93ca59f90c0f474
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2021-08-26 10:45:13 -0500

    Improves isPangram function.

--- pangram.js
+++ pangram.js
@@ -1,20 +1,21 @@
 
 export const isPangram = (str) => {
+  if (str === "" || str === undefined) return false
   /* 
    Create a new set from the string,
    transform the set to an array 
-   and verify if the length of the array 
-   is as long as the letters from the alphabet.
   */
   const arr = Array.from(new Set(str.replace(/[^a-z]/gi, "")))
-  if (arr.length !== 26) return false
   /* 
     Create a regular expression that match
-    the patterns required, then match that to the string
-    compare the first index of the array from the match method to the string
-    if the first index is not equal to the string, return false
+    the patterns required to check if a string is a pangram
   */
   const regex = /"?([A-Za-z][a-z]+\s?_?[0-9]?\s?)+\.?"?/g
-  if (str.match(regex)[0] === str) return true
+  /*
+    If the length of the array is equal to the length of the alphabet,
+    and the string matches the regular expression
+    then the string is a pangram
+  */
+  if (arr.length === 26 && str.match(regex)[0] === str) return true
   return false
 }
