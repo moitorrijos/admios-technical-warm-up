78f8c46b24f11638d022d8d7c2f1cf80c59a91c5
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2021-08-26 09:25:49 -0500

    Fixes regex and adds a verification to pass all tests.

--- pangram.js
+++ pangram.js
@@ -1,16 +1,20 @@
 
-export const isPangram = (string) => {
+export const isPangram = (str) => {
   /* 
    Create a new set from the string,
    transform the set to an array 
    and verify if the length of the array 
    is as long as the letters from the alphabet.
   */
-  const array = Array.from(new Set(string.replace(/[^a-z]/g, '')));
-  if (array.length !== 26) return false
+  const arr = Array.from(new Set(str.replace(/[^a-z]/gi, "")))
+  if (arr.length !== 26) return false
   /* 
-    Create a regular expression that matches the following
+    Create a regular expression that match
+    the patterns required, then match that to the string
+    compare the first index of the array from the match method to the string
+    if the first index is not equal to the string, return false
   */
-  const regex = new RegExp(/.[a-z ]\w../, 'gi');
-  return regex.test(string)
-};
+  const regex = /"?([A-Za-z][a-z]+\s?_?[0-9]?\s?)+\.?"?/g
+  if (str.match(regex)[0] === str) return true
+  return false
+}
