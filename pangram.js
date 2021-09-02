5e1fe1f5208ff53c44559945740b98974a33f7e8
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2021-09-02 15:03:56 -0500

    The best code ever for this test!

--- pangram.js
+++ pangram.js
@@ -1,21 +1,7 @@
 
 export const isPangram = (str) => {
-  if (str === "" || str === undefined) return false
-  /* 
-   Create a new set from the string,
-   transform the set to an array 
-  */
-  const arr = Array.from(new Set(str.replace(/[^a-z]/gi, "")))
-  /* 
-    Create a regular expression that match
-    the patterns required to check if a string is a pangram
-  */
-  const regex = /"?([A-Za-z][a-z]+\s?_?[0-9]?\s?)+\.?"?/g
-  /*
-    If the length of the array is equal to the length of the alphabet,
-    and the string matches the regular expression
-    then the string is a pangram
-  */
-  if (arr.length === 26 && str.match(regex)[0] === str) return true
+  if (!str) return false
+  const set = new Set(str.toLowerCase().replace(/[^a-z]/gi, ""))
+  if (set.size === 26) return true
   return false
 }
